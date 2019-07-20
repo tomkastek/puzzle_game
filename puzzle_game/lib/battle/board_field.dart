d3f4b7ed3c461db0d0aa17c3bfda6271da4d8c79
Tom Kastek <tomkastek@gmail.com>
2019-07-20 22:06:49 +0200

    make orbs draggable

--- puzzle_game/lib/battle/board_field.dart
+++ puzzle_game/lib/battle/board_field.dart
@@ -17,26 +17,43 @@ class BoardField extends StatelessWidget {
   Widget build(BuildContext context) {
     final gridBloc = BlocProvider.of<GridBloc>(context);
 
-    return BlocBuilder(bloc: gridBloc, builder: (context, GridState state) {
-      int x = state.xPos(index);
-      int y = state.yPos(index);
-      var dark = x % 2 == 0 ? index % 2 == 0 : index % 2 == 1;
-      return Container(
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 0.5),
-            color: dark ? Colors.brown[500] : Colors.brown[700]),
-        child: Center(
-          child: Draggable(
-            child: BoardItem(
-              itemIdentifier: state.grid[x][y],
-            ),
-            feedback: BoardItem(
-              itemIdentifier: state.grid[x][y],
-              alpha: 10,
-            ),
-          ),
-        ),
-      );
-    });
+    return BlocBuilder(
+        bloc: gridBloc,
+        builder: (context, GridState state) {
+          int x = state.xPos(index);
+          int y = state.yPos(index);
+          var dark = x % 2 == 0 ? index % 2 == 0 : index % 2 == 1;
+          return Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 0.5),
+                color: dark ? Colors.brown[500] : Colors.brown[700]),
+            child: LayoutBuilder(builder: (context, constraints) {
+              return Draggable(
+                child: BoardItem(
+                  itemIdentifier: state.grid[x][y],
+                ),
+                feedback: Container(
+                  height: constraints.biggest.height * 1.1,
+                  width: constraints.biggest.width * 1.1,
+                  transform: Matrix4.translationValues(
+                      -(constraints.biggest.width * 1.1) / 2,
+                      -(constraints.biggest.height * 1.1) / 1.5,
+                      0),
+                  child: BoardItem(
+                    itemIdentifier: state.grid[x][y],
+                  ),
+                ),
+                childWhenDragging: BoardItem(
+                  itemIdentifier: state.grid[x][y],
+                  alpha: 50,
+                ),
+                dragAnchor: DragAnchor.pointer,
+                onDragStarted: () {
+                  print('set state different here');
+                },
+              );
+            }),
+          );
+        });
   }
 }
